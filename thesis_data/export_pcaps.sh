ce8107aeb1bffd8aeefe2b6d4bab7ae006c07b49
PETHUN9 <pethun9@gmail.com>
2023-02-18 11:46:02 +0100

    csv shenanigans

--- thesis_data/export_pcaps.sh
+++ thesis_data/export_pcaps.sh
@@ -9,10 +9,10 @@ for I_KBIT in ${KBIT[@]}; do
         for FILE_NUM in {1..10}; do
             UDP_STREAM=$(tshark -nr caps/${I_KBIT}_${I_LOSS}_${FILE_NUM}.pcapng -q -z io,stat,1,ip.addr==66.22.244.132 | grep '<>' | sed 's/^|* ... <> ... | *//' | sed 's/ | ..... . |$//')
             TCP_STREAM=$(tshark -nr caps/${I_KBIT}_${I_LOSS}_${FILE_NUM}.pcapng -q -z 'io,stat,1,ip.addr==192.168.2.31&&ip.addr==162.159.0.0/16' | grep '<>' | sed 's/^|* ... <> ... | * .. | *//' | sed 's/ | * |$//')
-            echo $UDP_STREAM | tee -a csv/udp/${I_KBIT}_${I_LOSS}_udp.xlsx
-            echo $TCP_STREAM | tee -a csv/tcp/${I_KBIT}_${I_LOSS}_tcp.xlsx
+            echo $UDP_STREAM | tee -a csv/udp/${I_KBIT}_${I_LOSS}_udp.csv
+            echo $TCP_STREAM | tee -a csv/tcp/${I_KBIT}_${I_LOSS}_tcp.csv
         done
-        sed -i 's/ /;/g' csv/udp/${I_KBIT}_${I_LOSS}_udp.xlsx
-        sed -i 's/ /;/g' csv/tcp/${I_KBIT}_${I_LOSS}_tcp.xlsx
+        sed -i 's/ /;/g' csv/udp/${I_KBIT}_${I_LOSS}_udp.csv
+        sed -i 's/ /;/g' csv/tcp/${I_KBIT}_${I_LOSS}_tcp.csv
     done
 done
