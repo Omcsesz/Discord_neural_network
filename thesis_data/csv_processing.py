26ab3bc636798365f38e3a3142289299ab794490
PETHUN9 <pethun9@gmail.com>
2023-02-27 14:53:05 +0100

    Code refactoring for better visibility

---
+++ thesis_data/csv_processing.py
@@ -0,0 +1,84 @@
+
+import logging
+import os
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+logging.basicConfig(level=logging.DEBUG)
+_logger = logging.getLogger(__name__)
+def TimeSinceLastZero(input_dict):
+    data_help = input_dict[1.5].to_numpy()  # numpy array only used for getting size of array
+    TimeSinceLastX = np.empty([np.shape(data_help)[0], np.shape(data_help)[1]])
+    TimeSinceLastDict = {}
+
+    for key, input_table in input_dict.items():
+        df = pd.DataFrame(data=input_table)
+        for row_no, row in df.iterrows(): # iterating in rows where row_no is the row number, row is the data in a whole row
+            col_iter = 0  # column iterator, 0 at the beginning of each row
+            TSLX = 0
+            for cell in row:  # iterating in columns where cell is one cell of the row
+                if cell == 0:
+                    TSLX += 1
+                else:
+                    TSLX = 0
+                #if input_table.equals(discord_ctrl_bad) or input_table.equals(discord_ctrl_good): # counting bits or packets
+                #    if cell == 0: # if there are no bits/s TSLB counts up otherwise it's 0
+                #        TSLX += 1
+                #    else:
+                #        TSLX = 0
+                #else:
+                #    if cell < 40:  # if there are less than 40 packets/s TSLP counts up otherwise it's 0
+                #        TSLX += 1
+                #    else:
+                #        TSLX = 0
+                TimeSinceLastX[row_no, col_iter] = TSLX  # filling the result table used later
+                col_iter += 1
+        TimeSinceLastDict[key] = TimeSinceLastX
+    return TimeSinceLastDict
+
+
+# Importing data files
+def Import_Files():
+    kbit = [ 80, 90, 100, 0 ]
+    loss = [ 10, 0 ]
+    udp_data = {}
+    tcp_data = {}
+    quality = 1.5
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    for i_kbit in kbit:
+        for i_loss in loss:
+#            if i_kbit == 70 and i_loss == 10:
+#                continue
+            udp_data[quality] = pd.read_csv(f"{current_dir}/csv/udp/{i_kbit}_{i_loss}_udp.csv", sep=';', header=None)
+            tcp_data[quality] = pd.read_csv(f"{current_dir}/csv/tcp/{i_kbit}_{i_loss}_tcp.csv", sep=';', header=None)
+            quality += 0.5
+    return udp_data, tcp_data
+
+def process():
+    udp_data, tcp_data = Import_Files()
+    TimeSinceLastPacket = TimeSinceLastZero(udp_data)
+    TimeSinceLastByte = TimeSinceLastZero(tcp_data)
+    discord = pd.DataFrame()  # final DataFrames into which the preprocessed data goes
+    # discord_bad = pd.DataFrame()
+
+    for key in udp_data:
+        for i in range(udp_data[key].shape[0]):
+            discord_help = pd.DataFrame()
+            discord_help[0] = udp_data[key].to_numpy()[i]
+            discord_help[1] = TimeSinceLastPacket[key][i]
+            discord_help[2] = tcp_data[key].to_numpy()[i]
+            discord_help[3] = TimeSinceLastByte[key][i]
+            discord_help[4] = key
+            discord = pd.concat([discord, discord_help], ignore_index=True)
+
+    discord.columns = ['voice', 'TSLP', 'ctrl', 'TSLB', 'quality']
+    X = discord.iloc[:, 0:4]
+    y = np.ravel(discord.quality)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    scaler = StandardScaler().fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+    return X_train, X_test, y_train, y_test
---
+++ thesis_data/main.py
@@ -0,0 +1,38 @@
+
+import logging
+from keras.models import Sequential
+from keras.layers import Dense
+from csv_processing import process
+
+
+logging.basicConfig(level=logging.DEBUG)
+_logger = logging.getLogger(__name__)
+def main():
+    try:
+        X_train, X_test, y_train, y_test = process()
+        model = Sequential()
+        model.add(Dense(12, activation='relu', input_shape=(X_train.shape[1],)))
+        model.add(Dense(20, activation='relu'))
+        model.add(Dense(1, activation='sigmoid'))
+
+        model.compile(loss='binary_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+
+        model.fit(X_train, y_train, epochs=20, batch_size=133, verbose=1)
+
+        y_pred = model.predict(X_test)
+
+        for i in range(100):
+            print(y_pred[i])
+
+        score = model.evaluate(X_test, y_test, verbose=1)
+
+        _logger.info(score)
+        return 0
+    except Exception as e:
+        _logger.error(e)
+        return 1
+
+if __name__ == "__main__":
+    exit(main())
