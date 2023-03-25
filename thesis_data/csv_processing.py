e0502357d56610f5fbe578a42455be34f98a3174
PETHUN9 <pethun9@gmail.com>
2023-03-25 16:39:30 +0100

    Working on tcp_len and retransmissions

--- thesis_data/csv_processing.py
+++ thesis_data/csv_processing.py
@@ -1,11 +1,13 @@
+import csv
 import logging
 import os
+import subprocess
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG) # format='%(levelname) [%(filename):%(lineno)] %(message)'
 _logger = logging.getLogger(__name__)
 
 
@@ -18,10 +20,6 @@ def TimeSinceLastZero(input_table, s_type):
         col_iter = 0  # column iterator, 0 at the beginning of each row
         TSLX = 0
         for cell in row:  # iterating in columns where cell is one cell of the row
-            # if cell == 0:
-            #     TSLX += 1
-            # else:
-            #     TSLX = 0
             if s_type == "tcp": # counting bytes or packets
                 if cell == 0: # if there are no bits/s TSLB counts up otherwise it's 0
                     TSLX += 1
@@ -41,6 +39,7 @@ def TimeSinceLastZero(input_table, s_type):
 def Import_Files():
     kbit = [80, 90, 100, 0]
     loss = [10, 0]
+    stream_type = ["udp", "tcp"]
     udp_data = {}
     tcp_data = {}
     quality = 1.5
@@ -49,12 +48,36 @@ def Import_Files():
         for i_loss in loss:
             #            if i_kbit == 70 and i_loss == 10:
             #                continue
+            for s_type in stream_type:
+                with open(f"{current_dir}/csv/{s_type}/{i_kbit}_{i_loss}_{s_type}.csv", 'r') as rf:
+                    reader = csv.reader(rf, delimiter=';')
+                    num_cols = len(reader.__next__())
+                    num_keep_cols = min(num_cols, 130)
+                    keep_cols = list(range(num_keep_cols))
+                    rf.seek(0)
+
+                    with open(f"{current_dir}/csv/{s_type}/{i_kbit}_{i_loss}_{s_type}_m.csv", 'w', newline='') as wf:
+                        writer = csv.writer(wf, delimiter=';')
+                        for row in reader:
+                            writer.writerow([row[i] for i in keep_cols])
+
             udp_data[quality] = {}
             tcp_data[quality] = {}
-            udp_data[quality]['data_stream'] = pd.read_csv(f"{current_dir}/csv/udp/{i_kbit}_{i_loss}_udp.csv", sep=';', header=None)
+            retran = pd.read_csv(f"{current_dir}/test/csv/tcp/{i_kbit}_{i_loss}_retran.csv", sep=';', header=None)
+            retran = retran.fillna(0)
+            tcp_len = pd.DataFrame(0, index=range(130), columns=range(1))
+            retransmissions = pd.DataFrame(0, index=range(130), columns=range(1))
+            for row_no, row in retran.iterrows():
+                if int(row[0]) >= 130:
+                    continue
+                tcp_len[0][int(row[0])] += row[1]
+                retransmissions[0][int(row[0])] += row[2]
+            tcp_data[quality]['retransmission'] = retransmissions
+            tcp_data[quality]['tcp_len'] = tcp_len
+            udp_data[quality]['data_stream'] = pd.read_csv(f"{current_dir}/csv/udp/{i_kbit}_{i_loss}_udp_m.csv", sep=';', header=None)
             udp_data[quality]['kbit'] = i_kbit
             udp_data[quality]['loss'] = i_loss
-            tcp_data[quality]['data_stream'] = pd.read_csv(f"{current_dir}/csv/tcp/{i_kbit}_{i_loss}_tcp.csv", sep=';', header=None)
+            tcp_data[quality]['data_stream'] = pd.read_csv(f"{current_dir}/csv/tcp/{i_kbit}_{i_loss}_tcp_m.csv", sep=';', header=None)
             tcp_data[quality]['kbit'] = i_kbit
             tcp_data[quality]['loss'] = i_loss
             quality += 0.5
@@ -66,12 +89,13 @@ def process():
         udp_data, tcp_data = Import_Files()
         TimeSinceLastPacket = {}
         TimeSinceLastByte = {}
+        TimeSinceLastRetran = {}
         for key, input_table in udp_data.items():
             TimeSinceLastPacket[key] = TimeSinceLastZero(input_table['data_stream'], "udp")
         for key, input_table in tcp_data.items():
             TimeSinceLastByte[key] = TimeSinceLastZero(input_table['data_stream'], "tcp")
+            TimeSinceLastRetran[key] = TimeSinceLastZero(input_table['retransmission'], "tcp")
         discord = pd.DataFrame()  # final DataFrames into which the preprocessed data goes
-        # discord_bad = pd.DataFrame()
 
         for key in udp_data:
             for i in range(udp_data[key]['data_stream'].shape[0]):
@@ -82,12 +106,15 @@ def process():
                 discord_help[3] = TimeSinceLastByte[key][i]
                 discord_help[4] = udp_data[key]['kbit']
                 discord_help[5] = udp_data[key]['loss']
-                discord_help[6] = (key-1)/4
+                discord_help[6] = tcp_data[key]['retransmission']
+                discord_help[6] = TimeSinceLastRetran[key][i]
+                discord_help[7] = tcp_data[key]['tcp_len']
+                discord_help[8] = (key - 1) / 4
                 discord = pd.concat([discord, discord_help], ignore_index=True)
 
-        discord.columns = ['voice', 'TSLP', 'ctrl', 'TSLB', 'kbit', 'loss', 'quality']
+        discord.columns = ['voice', 'TSLP', 'ctrl', 'TSLB', 'kbit', 'loss', 'retransmissions', 'tcp_len', 'quality']
         discord.to_csv(f'{os.path.dirname(os.path.realpath(__file__))}/neural_network_inputs.csv', sep=',')
-        X = discord.iloc[:, 0:6]
+        X = discord.iloc[:, 0:8]
         y = np.ravel(discord.quality)
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
         scaler = StandardScaler().fit(X_train)
--- thesis_data/export_pcaps.sh
+++ thesis_data/export_pcaps.sh
@@ -4,15 +4,23 @@ set -veu
 KBIT=(80 90 100 0)
 LOSS=(0 10)
 
+rm test/csv/udp/*
+rm test/csv/tcp/*
+
 for I_KBIT in ${KBIT[@]}; do
     for I_LOSS in ${LOSS[@]}; do
         for FILE_NUM in {1..10}; do
             UDP_STREAM=$(tshark -nr caps/${I_KBIT}_${I_LOSS}_${FILE_NUM}.pcapng -q -z io,stat,1,ip.addr==66.22.244.132 | grep '<>' | sed 's/^|* ... <> ... | *//' | sed 's/ | ..... . |$//')
             TCP_STREAM=$(tshark -nr caps/${I_KBIT}_${I_LOSS}_${FILE_NUM}.pcapng -q -z 'io,stat,1,ip.addr==192.168.2.31&&ip.addr==162.159.0.0/16' | grep '<>' | sed 's/^|* ... <> ... | * .. | *//' | sed 's/ | * |$//')
-            echo $UDP_STREAM | tee -a csv/udp/${I_KBIT}_${I_LOSS}_udp.csv
-            echo $TCP_STREAM | tee -a csv/tcp/${I_KBIT}_${I_LOSS}_tcp.csv
+            RETRAN=$(tshark -nr caps/${I_KBIT}_${I_LOSS}_${FILE_NUM}.pcapng -Y 'ip.addr==192.168.2.31&&ip.addr==162.159.0.0/16' -T fields -e frame.time_relative -e tcp.len -e tcp.analysis.retransmission) # > test/csv/tcp/${I_KBIT}_${I_LOSS}_retran.csv
+            echo $UDP_STREAM | tee -a test/csv/udp/${I_KBIT}_${I_LOSS}_udp.csv
+            echo $TCP_STREAM | tee -a test/csv/tcp/${I_KBIT}_${I_LOSS}_tcp.csv
+            echo $RETRAN | tee -a test/csv/tcp/${I_KBIT}_${I_LOSS}_retran.csv
         done
-        sed -i 's/ /;/g' csv/udp/${I_KBIT}_${I_LOSS}_udp.csv
-        sed -i 's/ /;/g' csv/tcp/${I_KBIT}_${I_LOSS}_tcp.csv
+        sed -i 's/ /;/g' test/csv/tcp/${I_KBIT}_${I_LOSS}_retran.csv
+        sed -i 's/ /;/g' test/csv/udp/${I_KBIT}_${I_LOSS}_udp.csv
+        sed -i 's/ /;/g' test/csv/tcp/${I_KBIT}_${I_LOSS}_tcp.csv
     done
 done
+
+# tshark -nr caps/80_10_10.pcapng -Y 'ip.addr==192.168.2.31&&ip.addr==162.159.0.0/16' -T fields -e frame.time_relative -e tcp.analysis.retransmission
